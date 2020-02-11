f8453604df13a8699381a0a28665dcc6f8d82b5b
Ryan Leach <rwmleach@gmail.com>
2020-02-11 10:35:56 -0500

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Ryan Leach on 2/11/20.
+//  Copyright © 2020 Ryan Leach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
